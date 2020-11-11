eb65835248210fa15721ec74932297d07a1f4c59
Mahdi Farra <me@mahdif.com>
2020-11-10 18:17:29 -0800

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="home">
-    <div class="main_wrapper">
-      <h2>Emoji Picker</h2>
-      <EmojiPicker />
-    </div>
+  <div class="main_wrapper">
+    <h2>Emoji Picker</h2>
+    <EmojiPicker />
+    <hr>
+    <h2>Reactions</h2>
+    <Reactions />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import EmojiPicker from '@/components/EmojiPicker.vue'
+import Reactions from '@/components/Reactions.vue'
 
 export default {
   name: 'Home',
   components: {
-    EmojiPicker
+    EmojiPicker,
+    Reactions,
   }
 }
 </script>
